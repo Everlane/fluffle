14c6ff0c25e5bf2da8a7a275c711446ce6976229
Dirk Gadsden <dirk@esherido.com>
2016-08-08 23:28:39 -0700

    Add helper module methods for composing fully-qualified queue names

--- lib/fluffle.rb
+++ lib/fluffle.rb
@@ -6,3 +6,14 @@ require 'fluffle/handlers/base'
 require 'fluffle/handlers/delegator'
 require 'fluffle/handlers/dispatcher'
 require 'fluffle/server'
+
+module Fluffle
+  # Expand a short name into a fully-qualified one
+  def self.request_queue_name(name)
+    "fluffle.requests.#{name}"
+  end
+
+  def self.response_queue_name(name)
+    "fluffle.responses.#{name}"
+  end
+end
