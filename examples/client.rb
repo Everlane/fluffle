9abf04ed0a51cca08aed34acb70c93faa613b391
Dirk Gadsden <dirk@esherido.com>
2016-08-08 23:30:30 -0700

    Example client and server code

---
+++ examples/client.rb
@@ -0,0 +1,14 @@
+lib = File.join File.dirname(__FILE__), '..', 'lib'
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+
+require 'fluffle'
+
+server = Fluffle::Client.new url: 'amqp://localhost'
+
+timings = 10.times.map do
+  t0 = Time.now
+  server.call('foo').inspect
+  Time.now - t0
+end
+
+puts timings
---
+++ examples/server.rb
@@ -0,0 +1,12 @@
+lib = File.join File.dirname(__FILE__), '..', 'lib'
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+
+require 'fluffle'
+
+server = Fluffle::Server.new url: 'amqp://localhost'
+
+server.drain 'default' do |dispatcher|
+  dispatcher.handle('foo') { 'bar' }
+end
+
+server.start
